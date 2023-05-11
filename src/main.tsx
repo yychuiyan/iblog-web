90d2908e844a51f853535b060206d2c336f8b0f4
yychuiyan <yychuiyan@outlook.com>
2023-05-11 19:35:00 +0800

    feat:服务部署

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import 'flowbite';
 import './main.css';
 import { ConfigProvider } from 'antd';
 import zh_CN from 'antd/es/locale/zh_CN';
-// 主入口文件
+// 主入口
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ConfigProvider locale={zh_CN}>
     <App />
