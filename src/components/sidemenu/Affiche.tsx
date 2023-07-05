367c254acd8bd944f807cfc347290bbc3852329e
yychuiyan <yychuiyan@outlook.com>
2023-07-06 00:02:33 +0800

    feat:移动端公告

--- src/components/sidemenu/Affiche.tsx
+++ src/components/sidemenu/Affiche.tsx
@@ -61,12 +61,12 @@ const Affiche = (props: any) => {
 
       </div>
       {/* 移动端 */}
-      <div className="hidden lg:block lg:relative top-2 lg:h-24 lg:mx-auto lg:px-1  lg:rounded-xl lg:bg-base-100 lg:w-[calc(100%-30px)] sm:w-[calc(100%-50px)]">
-        <svg className="icon w-7 h-7 absolute -left-2 -top-2 transform rotate-12" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="34085"><path d="M163.84 496.14h558.74V900.3L333.72 638.5l-169.88 1.55V496.14z" fill="#FDD668" p-id="34086"></path><path d="M906.53 503.22c0-90.22-71-163.52-159.94-168.67V64L277.36 342.58l-137.52-27.77v376.82l137.52-27.77 469.23 278.6V671.89c88.97-5.15 159.94-78.45 159.94-168.67zM187.84 373.47l69.82 14.11v231.28L187.84 633z m510.75 484.67L305.66 624.85V381.6l392.93-233.3z m48-234.25V382.55c62.49 5.05 111.94 56.92 111.94 120.67s-49.45 115.63-111.94 120.67z" fill='var(--bgcolor-social-default)' p-id="34087"></path></svg>
+      <div className="hidden lg:block lg:relative top-2 lg:h-24 lg:mx-auto   lg:rounded-xl lg:bg-base-100 lg:w-[calc(100%-30px)] sm:w-[calc(100%-45px)]">
+        <svg className="icon w-7 h-7 absolute -left-3 -top-3 transform rotate-12" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="34085"><path d="M163.84 496.14h558.74V900.3L333.72 638.5l-169.88 1.55V496.14z" fill="#FDD668" p-id="34086"></path><path d="M906.53 503.22c0-90.22-71-163.52-159.94-168.67V64L277.36 342.58l-137.52-27.77v376.82l137.52-27.77 469.23 278.6V671.89c88.97-5.15 159.94-78.45 159.94-168.67zM187.84 373.47l69.82 14.11v231.28L187.84 633z m510.75 484.67L305.66 624.85V381.6l392.93-233.3z m48-234.25V382.55c62.49 5.05 111.94 56.92 111.94 120.67s-49.45 115.63-111.94 120.67z" fill='var(--bgcolor-social-default)' p-id="34087"></path></svg>
         <Carousel autoplay autoplaySpeed={5000} dotPosition="left" dots={false} className='lg:px-3'>
           {
             list.map((item: DataType) => (
-              <p className='lg:pt-[5px] lg:px-2 lg:h-20 lg:text-base lg:text-[var(--color-font-color)] lg:overflow-hidden' >
+              <p className='lg:pt-[5px] lg:h-20  lg:text-base lg:text-[var(--color-font-color)] lg:overflow-y-hidden' >
                 <LinesEllipsis
                   text={item.content}
                   maxLine="3"
