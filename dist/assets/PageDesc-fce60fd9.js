43926ad6ddd17b84031289589e3bb6e3ca3d6795
yychuiyan <yychuiyan@outlook.com>
2023-08-18 00:26:51 +0800

    feat:新增天气

---
+++ dist/assets/PageDesc-fce60fd9.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-3a328f3e.js";const l=t=>e("div",{className:"flex justify-center items-center flex-col w-full h-32 mt-20 sm:h-20",children:e("p",{className:"text-3xl font-medium ",style:{userSelect:"none"},children:t.title})});export{l as P};
