3c32df3d34fd8837f85a4ea9f99b5ac7d26effad
夜雨炊烟 <yychuiyan@outlook.com>
2024-08-17 15:40:03 +0800

    fix: 时间线和首页开屏变更

--- dist/assets/PageDesc-4f817876.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/PageDesc-4706c239.js
-import{p as s,q as t}from"./index-2d9379ef.js";const c=e=>s("div",{className:"flex justify-center items-center flex-col w-full h-32 mt-20 sm:h-20",children:[t("p",{className:"text-3xl font-medium ",style:{userSelect:"none"},children:e.title}),t("p",{className:"mt-2 text-[var(--article-content-default)]",children:e.desc})]});export{c as P};
-========
-import{p as s,q as t}from"./index-19447457.js";const c=e=>s("div",{className:"flex justify-center items-center flex-col w-full h-32 mt-20 sm:h-20",children:[t("p",{className:"text-3xl font-medium ",style:{userSelect:"none"},children:e.title}),t("p",{className:"mt-2 text-[var(--article-content-default)]",children:e.desc})]});export{c as P};
->>>>>>>> develop:dist/assets/PageDesc-4f817876.js
